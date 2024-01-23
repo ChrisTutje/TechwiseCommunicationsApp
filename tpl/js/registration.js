fc38e501852c46df32d69788ef4398a2a4f29eda
Marlo-Ong <moon.ongkingco@gmail.com>
2024-01-22 18:30:17 -0800

    Update registration page

--- tpl/js/registration.js
+++ tpl/js/registration.js
@@ -1,39 +1,70 @@
-const registrationForm = document.forms[0];
-const loginButton = registrationForm[3];
+const loginForm = document.forms[0];
+const registrationForm = document.forms[1];
+const loginButton = loginForm[2];
 const registerButton = registrationForm[4];
 
-function validateEligibility(event) {
+function validateRegistration(event) {
   event.preventDefault();
   if (registrationFormInputs.some(isEmpty)) {
     alert("You must fill in every field.");
   }
+  else if (!isStrongPassword(passwordInput.value)) {
+    alert("Your password must contain at least one of each: \
+    Capital letter, lowercase letter, number, and special character.");
+  }
   else if (passwordInput.value !== confirmPasswordInput.value) {
     alert("Your passwords must match.");
   }
+  else if (!disclaimerCheckbox.checked){
+    alert("You must read and accept the disclaimer.");
+  }
+  else {
+    insertUser(usernameInput.value, passwordInput.value);
+  }
 }
-registerButton.addEventListener("click", validateEligibility);
+
+function validateLogin(event) {
+  event.preventDefault();
+  if (isEmpty(usernameInputLogin) || isEmpty(passwordInputLogin)) {
+    alert("You must fill in every field.");
+  }
+  else {
+    //loginUser(usernameInput.value, passwordInput.value);
+  }
+}
+
+function validateUser(event) {
+  event.preventDefault();
+  console.log("is user good?");
+}
+
+registerButton.addEventListener("click", validateRegistration);
+loginButton.addEventListener("click", validateLogin);
 
 function isEmpty(input) {
   return input.value == "";
 }
 
+function isStrongPassword(string) {
+  return /\d/.test(string) // number
+    && /[!@#$%^&*()<>?,."';:{}]/.test(string) // special character
+    && /[A-Z]/.test(string) // capital letter
+    && /[a-z]/.test(string); // lowercase letter
+}
+
+const usernameInputLogin = loginForm[0];
+const passwordInputLogin = loginForm[1];
+
 const usernameInput = registrationForm[0];
 const passwordInput = registrationForm[1];
 const confirmPasswordInput = registrationForm[2];
-const securityQuestionInput = registrationForm[3];
-const securityAnswerInput = registrationForm[4];
+const disclaimerCheckbox = registrationForm[3];
 
 const registrationFormInputs = [
   usernameInput,
   passwordInput,
   confirmPasswordInput,
-  // securityQuestionInput,
-  // securityAnswerInput,
 ];
-
-function passwordsMatch() {
-  return ;
-}
 
 function legalCheckboxChecked() {
   return ;
