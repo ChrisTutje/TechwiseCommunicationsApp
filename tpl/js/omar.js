27183797117b090b2601e010276ca7ceebdaec82
Torirem03 <113369584+Torirem03@users.noreply.github.com>
2024-01-29 22:12:51 -0600

    preliminary omar
    huge library install and nodes for npm. omar is working with just 1 step for now. adding more steps later

--- tpl/js/omar.js
+++ tpl/js/omar.js
@@ -1,3 +1,43 @@
 /* Helper Program that will guide new users through the site. Codename OMAR
 Amber Miller 
 */
+import Shepherd from "shepherd.js";
+
+function OmarTime(){
+console.log("hi omar");
+const tour = new Shepherd.Tour({
+	defaultStepOptions: {
+		cancelIcon: {
+			enabled: true
+		},
+		classes: 'class-1 class-2',
+		scrollTo: { behavior: 'smooth', block: 'center' }
+	}
+});
+		
+tour.addStep({
+    title: 'Creating a Shepherd Tour',
+	text: 'Creating a Shepherd tour is easy. too! Just create a "Tour" instance, and add as many steps as you want.',
+	attachTo: {
+		element: '.omar',
+		on: 'bottom'
+    },
+	buttons: [
+		{
+		action() {
+			return this.back();
+		},
+		classes: 'shepherd-button-secondary',
+		text: 'Back'
+		},
+		{
+			action() {
+				return this.next();
+			},
+			text: 'Next'
+		}
+	],
+	id: 'creating'
+});
+		
+tour.start();}
