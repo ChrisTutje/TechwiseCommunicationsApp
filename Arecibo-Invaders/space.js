28a0e6ede5c841ab6c71cac12fed517260ad2ed5
Eric-Holman <138829227+Eric-Holman@users.noreply.github.com>
2024-02-04 19:27:41 -0500

    Update to space shooter

--- Arecibo-Invaders/space.js
+++ Arecibo-Invaders/space.js
@@ -1,7 +1,7 @@
 //board
 let tileSize = 32;
-let rows = 16;
-let columns = 20;
+let rows = 23;
+let columns = 47;
 
 let board;
 let boardWidth = tileSize * columns; // 32 * 16
@@ -9,34 +9,41 @@ let boardHeight = tileSize * rows; // 32 * 16
 let context;
 
 //ship
-let shipWidth = tileSize*1.5;
+let shipWidth = tileSize * 1.5;
 let shipHeight = tileSize;
-let shipX = tileSize * columns/2 - tileSize;
-let shipY = tileSize * rows - tileSize*2;
+let shipX = tileSize * columns / 2 - tileSize;
+let shipY = tileSize * rows - tileSize * 2;
 
 let ship = {
-    x : shipX,
-    y : shipY,
-    width : shipWidth,
-    height : shipHeight
+    x: shipX,
+    y: shipY,
+    width: shipWidth,
+    height: shipHeight
 }
-
 
 let shipImg;
 let shipVelocityX = tileSize; //ship moving speed
 
 //aliens
 let alienArray = [];
-let alienWidth = tileSize*2;
+let alienWidth = tileSize * 2;
 let alienHeight = tileSize;
 let alienX = tileSize;
 let alienY = tileSize;
-let alienImg;
 
-let alienRows = 2;
-let alienColumns = 3;
+// Array of alien image sources
+const alienImageSources = [
+    "./alien.png",
+    "./alien-yellow.png",
+    "./alien-cyan.png",
+    "./alien-magenta.png",
+    // Add more image sources as needed
+];
+
+let alienRows = 3;
+let alienColumns = 6;
 let alienCount = 0; //number of aliens to defeat
-let alienVelocityX = 1; //alien moving speed
+let alienVelocityX = 1.5; //alien moving speed
 
 //bullets
 let bulletArray = [];
@@ -45,11 +52,11 @@ let bulletVelocityY = -10; //bullet moving speed
 let score = 0;
 let gameOver = false;
 
-window.onload = function() {
+window.onload = function () {
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight;
-    context = board.getContext("2d"); //used for drawing on the board
+    context = board.getContext("2d");
 
     //draw initial ship
     // context.fillStyle="green";
@@ -58,12 +65,10 @@ window.onload = function() {
     //load images
     shipImg = new Image();
     shipImg.src = "./ship.png";
-    shipImg.onload = function() {
+    shipImg.onload = function () {
         context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     }
 
-    alienImg = new Image();
-    alienImg.src = "./alien.png";
     createAliens();
 
     requestAnimationFrame(update);
@@ -92,14 +97,14 @@ function update() {
             //if alien touches the borders
             if (alien.x + alien.width >= board.width || alien.x <= 0) {
                 alienVelocityX *= -1;
-                alien.x += alienVelocityX*2;
+                alien.x += alienVelocityX * 2;
 
                 //move all aliens up by one row
                 for (let j = 0; j < alienArray.length; j++) {
                     alienArray[j].y += alienHeight;
                 }
             }
-            context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height);
+            context.drawImage(alien.img, alien.x, alien.y, alien.width, alien.height);
 
             if (alien.y >= ship.y) {
                 gameOver = true;
@@ -111,7 +116,7 @@ function update() {
     for (let i = 0; i < bulletArray.length; i++) {
         let bullet = bulletArray[i];
         bullet.y += bulletVelocityY;
-        context.fillStyle="white";
+        context.fillStyle = "white";
         context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
 
         //bullet collision with aliens
@@ -135,12 +140,11 @@ function update() {
     if (alienCount == 0) {
         //increase the number of aliens in columns and rows by 1
         score += alienColumns * alienRows * 100; //bonus points :)
-        alienColumns = Math.min(alienColumns + 1, columns/2 -2); //cap at 16/2 -2 = 6
-        alienRows = Math.min(alienRows + 1, rows-4);  //cap at 16-4 = 12
+        alienColumns = Math.min(alienColumns + 1, columns / 2 - 2); //cap at 16/2 -2 = 6
+        alienRows = Math.min(alienRows + 1, rows - 4);  //cap at 16-4 = 12
         if (alienVelocityX > 0) {
             alienVelocityX += 0.2; //increase the alien movement speed towards the right
-        }
-        else {
+        } else {
             alienVelocityX -= 0.2; //increase the alien movement speed towards the left
         }
         alienArray = [];
@@ -149,8 +153,8 @@ function update() {
     }
 
     //score
-    context.fillStyle="white";
-    context.font="16px courier";
+    context.fillStyle = "white";
+    context.font = "16px courier";
     context.fillText(score, 5, 20);
 }
 
@@ -161,8 +165,7 @@ function moveShip(e) {
 
     if (e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0) {
         ship.x -= shipVelocityX; //move left one tile
-    }
-    else if (e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width) {
+    } else if (e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width) {
         ship.x += shipVelocityX; //move right one tile
     }
 }
@@ -170,14 +173,24 @@ function moveShip(e) {
 function createAliens() {
     for (let c = 0; c < alienColumns; c++) {
         for (let r = 0; r < alienRows; r++) {
+            // Randomly select an image source
+            const randomIndex = Math.floor(Math.random() * alienImageSources.length);
+            const randomImageSource = alienImageSources[randomIndex];
+
+            // Create an alien with the selected image source
             let alien = {
-                img : alienImg,
-                x : alienX + c*alienWidth,
-                y : alienY + r*alienHeight,
-                width : alienWidth,
-                height : alienHeight,
-                alive : true
-            }
+                img: new Image(),
+                x: alienX + c * alienWidth,
+                y: alienY + r * alienHeight,
+                width: alienWidth,
+                height: alienHeight,
+                alive: true
+            };
+
+            // Set the image source and load it
+            alien.img.src = randomImageSource;
+
+            // Push the alien to the array
             alienArray.push(alien);
         }
     }
@@ -192,11 +205,11 @@ function shoot(e) {
     if (e.code == "Space") {
         //shoot
         let bullet = {
-            x : ship.x + shipWidth*15/32,
-            y : ship.y,
-            width : tileSize/8,
-            height : tileSize/2,
-            used : false
+            x: ship.x + shipWidth * 15 / 32,
+            y: ship.y,
+            width: tileSize / 8,
+            height: tileSize / 2,
+            used: false
         }
         bulletArray.push(bullet);
     }
@@ -204,8 +217,7 @@ function shoot(e) {
 
 function detectCollision(a, b) {
     return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
-           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
-           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+        a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
+        a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
+        a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
 }
-
