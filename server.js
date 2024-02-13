74bbf4669aa3ad91306fde5cdd480ffd6d24548c
chris tutje <chris.tutje@gmail.com>
2024-02-12 22:24:10 -0600

    updated gitignore

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000; // Change this back to your desired port
+const PORT = process.env.SERVER_PORT;
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'Arecibo';
+const dbName = process.env.DATABASE_NAME;
 
 async function connectToDB() {
-  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-  return client.connect();
+  //const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/?retryWrites=true&w=majority`;
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  return await client.connect();
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/send', async (req, res) => {
+app.post("/message", async (req, res) => {
   let client;
 
   try {
@@ -28,14 +38,37 @@ app.post('/send', async (req, res) => {
     const body = req.body.body;
     const timestamp = new Date();
 
-    await db.collection('UserAccounts').insertOne({ sender });
-    await db.collection('Messages').insertOne({ sender, subject, body, timestamp });
+    await db.collection("UserAccounts").insertOne({ sender });
+    await db
+      .collection("Messages")
+      .insertOne({ sender, subject, body, timestamp });
 
-    //res.send('Message sent successfully!');
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+});
+
+app.get("/message", async (req, res) => {
+  let client;
+
+  try {
+    client = await connectToDB();
+    const db = client.db(dbName);
+
+    // Fetch messages from the Messages collection
+    const messages = await db.collection("Messages").find().toArray();
+
+    // Send messages as JSON response
+    res.json(messages);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   } finally {
     if (client) {
       await client.close();
@@ -48,39 +81,16 @@ app.use(express.static("public"));
 
 // Disable strict MIME checking globally
 app.use((req, res, next) => {
-  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader("X-Content-Type-Options", "nosniff");
   next();
 });
 
 // Serve index.html using a relative path
-app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, 'index.html');
+app.get("/", (req, res) => {
+  const indexPath = path.join(__dirname, "index.html");
   res.sendFile(indexPath);
-});
-
-app.get('/messages', async (req, res) => {
-  let client;
-
-  try {
-      client = await connectToDB();
-      const db = client.db(dbName);
-
-      // Fetch messages from the Messages collection
-      const messages = await db.collection('Messages').find().toArray();
-
-      // Send messages as JSON response
-      res.json(messages);
-  } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-      if (client) {
-          await client.close();
-      }
-  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
